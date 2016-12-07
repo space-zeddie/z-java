145d0fd37fa08dfcf9594559608ff9d60fdcd799
space-zeddie <thespacezeddie@gmail.com>
2016-12-07 15:20:38 +0200

    added static production stage

--- src/main/java/com/zakharuk/java/ZakharukApplication.java
+++ src/main/java/com/zakharuk/java/ZakharukApplication.java
@@ -7,7 +7,15 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @SpringBootApplication
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class ZakharukApplication {
+	private static int PRODUCTION_STAGE = 1;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ZakharukApplication.class, args);
+	}
+
+	public static void moveProductionStage(int s) {
+		if (s == 1 || s == 2 || s == 3)
+			PRODUCTION_STAGE = s;
+		else return;
 	}
 }
